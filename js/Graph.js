64f40cea2e1a0c0d00f988eb43e0ffa9e1a15fc1
dchang10 <dochang@wpi.edu>
2017-07-31 00:18:50 -0400

    Add labelsto graph
    Added labels to graph
    
    Added arrows to the end of the axes on the graph

--- js/Graph.js
+++ js/Graph.js
@@ -16,12 +16,12 @@ function Graph(config) {
         this.context = this.canvas.getContext('2d');
         this.rangeX = this.maxX - this.minX;
         this.rangeY = this.maxY - this.minY;
-        this.unitX = this.canvas.width / this.rangeX;
+        this.unitX = (this.canvas.width - 52) / this.rangeX;
         this.unitY = this.canvas.height / this.rangeY;
         this.centerY = Math.round(Math.abs(this.minY / this.rangeY) * this.canvas.height);
-        this.centerX = Math.round(Math.abs(this.minX / this.rangeX) * this.canvas.width);
+        this.centerX = Math.round(Math.abs(this.minX / this.rangeX) * (this.canvas.width - 52));
         this.iteration = (this.maxX - this.minX) / 1000;
-        this.scaleX = this.canvas.width / this.rangeX;
+        this.scaleX = (this.canvas.width - 52) / this.rangeX;
         this.scaleY = this.canvas.height / this.rangeY;
 
         // draw x and y axis
@@ -34,7 +34,7 @@ function Graph(config) {
         context.save();
         context.beginPath();
         context.moveTo(0, this.centerY);
-        context.lineTo(this.canvas.width, this.centerY);
+        context.lineTo((this.canvas.width - 52), this.centerY);
         context.strokeStyle = this.axisColor;
         context.lineWidth = 1;
         context.stroke();
@@ -43,8 +43,9 @@ function Graph(config) {
         var xPosIncrement = this.unitsPerTick * this.unitX;
         var xPos, unit;
         context.font = this.font;
-        context.fillStyle = 'blue';
+        context.fillStyle = 'white';
         context.textAlign = 'center';
+        context.font = "8pt Bubbler One"
         context.textBaseline = 'top';
 
         // draw left tick marks
@@ -62,14 +63,21 @@ function Graph(config) {
         // draw right tick marks
         xPos = this.centerX + xPosIncrement;
         unit = this.unitsPerTick;
-        while (xPos < this.canvas.width) {
+        while (xPos < (this.canvas.width - 52)) {
             context.moveTo(xPos, this.centerY - this.tickSize / 2);
             context.lineTo(xPos, this.centerY + this.tickSize / 2);
             context.stroke();
-            //context.fillText(unit, xPos, this.centerY + this.tickSize / 2 + 3);
+            context.fillText(unit, xPos, this.centerY + this.tickSize / 2 + 3);
             unit += this.unitsPerTick;
             xPos = Math.round(xPos + xPosIncrement);
         }
+        context.moveTo(this.canvas.width - 52, this.centerY + this.tickSize / 2 - 3);
+        context.lineTo(this.canvas.width - 55, this.centerY + this.tickSize / 2 - 5);
+        context.stroke(); 
+        context.moveTo(this.canvas.width - 52, this.centerY + this.tickSize / 2 - 3); 
+        context.lineTo(this.canvas.width - 55, this.centerY + this.tickSize / 2);
+        context.stroke(); 
+        context.fillText('r [Bohr radii]' , (this.canvas.width - 26), this.centerY + this.tickSize / 2 - 10);  
         context.restore();
     };
 
@@ -77,12 +85,23 @@ function Graph(config) {
         var context = this.context;
         context.save();
         context.beginPath();
-        context.moveTo(this.centerX, 0);
+        context.moveTo(this.centerX, 10);
         context.lineTo(this.centerX, this.canvas.height);
         context.strokeStyle = this.axisColor;
-        context.lineWidth = 2;
+        context.lineWidth = 1;
         context.stroke();
-
+        context.moveTo(this.centerX, 10 );
+        context.lineTo(this.centerX + 5, 15);
+        context.stroke();      
+        context.moveTo(this.centerX, this.canvas.height);
+        context.lineTo(this.centerX + 5, this.canvas.height - 5);
+        context.stroke();
+        context.font = this.font;
+        context.fillStyle = 'white';
+        context.textAlign = 'center';
+        context.font = "8pt Bubbler One"
+        context.textBaseline = 'top';
+        context.fillText("R (r)" , this.centerX + 8, 0);  
         context.restore();
     };
     Graph.prototype.update = function(minY,maxY){
@@ -103,7 +122,7 @@ function Graph(config) {
                 MY = temp;
             points.push([x,temp]);
         }
-        this.update(-MY*1.1, MY*1.1);
+        this.update(-MY*1.2, MY*1.2);
         context.save();
         context.save();
         this.transformContext();
